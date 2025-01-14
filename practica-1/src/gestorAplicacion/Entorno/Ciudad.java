60d974eb043b5902036876ee72e92598f1028180
danielh434 <dhincapieca@unal.edu.co>
2025-01-14 17:33:19 -0500

    Adendum - Entorno - DHC

--- practica-1/src/gestorAplicacion/Entorno/Ciudad.java
+++ practica-1/src/gestorAplicacion/Entorno/Ciudad.java
@@ -1,5 +1,46 @@
 package gestorAplicacion.Entorno;
 
-public class Ciudad {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Ciudad extends Zona implements Serializable {
+    //Atributos
+    private static final long serialVersionUID = 1L;
+    private static ArrayList<Ciudad> ciudades = new ArrayList<Ciudad>();
+    private ArrayList<Zona> zonasCiudad = new ArrayList<Zona>();
+
+    //Construtores
+    public Ciudad(){
+        ciudades.add(this);
+    }
+    public Ciudad(String nombre) {
+        this.nombre = nombre;
+        ciudades.add(this);
+    }
+
+    //Métodos
+    public ArrayList<Zona> getZonasCiudad() {
+        return zonasCiudad;
+    }
+    public static ArrayList<Ciudad> getCiudades() {
+        return ciudades;
+    }
+
+    public void actualizarPoblacion() {
+        this.poblacion = 0;
+        for (Zona zona : this.zonasCiudad) {
+            this.poblacion += (int) zona.getPoblacion();
+        }
+    }
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("{Información de la Ciudad: ");
+        sb.append("poblacion = ").append(poblacion);
+        sb.append(", nombre = '").append(nombre).append("'}");
+        return sb.toString();
+    }
+    public void agregarZonas(Zona zona){
+        zonasCiudad.add(zona);
+    }
 
 }
