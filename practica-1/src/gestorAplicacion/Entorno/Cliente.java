d7d6fc01fc466efce89a4901f5cd9bbb5a9c137b
valen1217 <102827620+valen1217@users.noreply.github.com>
2025-01-15 11:48:52 +0000

    Clase cliente y Reserva

---
+++ practica-1/src/gestorAplicacion/Entorno/Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente {
+    private String nombre;
+    private String email;
+
+    public Cliente(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
