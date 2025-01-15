4cabe8713781a7e32e14d2a4528ca4128b4e92cf
GHPaulina <pgomezh@unal.edu.co>
2025-01-14 20:27:59 -0500

    Lógica avance

--- practica-1/src/gestorAplicacion/Usuario/Persona.java
+++ practica-1/src/gestorAplicacion/Usuario/Persona.java
@@ -1,5 +1,50 @@
 package gestorAplicacion.Usuario;
 
-public abstract class Persona {
+import java.io.Serializable;
 
+public abstract class Persona implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	//ATRIBUTOS
+	
+	 protected String nombre;
+	 protected int cedula;
+	 
+	 //Como es una clase abstracta necesita un metodo abstracto
+	 
+	 //METODO ABSTRACTO
+	 
+	 public abstract void mostrarInformacion();
+	 
+	 //METODOS
+	 
+	 public String despedida() {
+	        return "Hasta luego " + nombre + ".\nEsperamos que regreses pronto."; // NOTA DE MI PARA MI: no me gusta esta despedida
+	        
+	 }
+	 
+	//GETTERS Y SETTERS 
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+
+	 
 }
+	 
+	 
+
+
