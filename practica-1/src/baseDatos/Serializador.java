b7656922caa2f7d25f06894ce7ec367f3eda2c21
danielh434 <dhincapieca@unal.edu.co>
2025-01-21 19:52:00 -0500

    Serializador - Ademdun

--- practica-1/src/baseDatos/Serializador.java
+++ practica-1/src/baseDatos/Serializador.java
@@ -1,28 +1,91 @@
 package baseDatos;
 
-//DHC - Falta serializar listas!!!
+import gestorAplicacion.paquete1.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
-import java.io.*;
-import java.util.ArrayList;
 
-public class Serializador { //Se encargara de agregar y serializar los objetos creados mientras corre.   
 
-    public static void serializar(ArrayList<? extends Object> lista, String nombre) {
-        File archivo = new File("");
-        try {
-            File ruta = new File(archivo.getAbsolutePath()+"/src/baseDatos/temp/"+nombre+".txt");
-            FileOutputStream fileOut = new FileOutputStream(ruta);
-            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-            objectOut.writeObject(lista);
-            objectOut.close();
-            fileOut.close();
-            System.out.println("El objeto ha sido serializado");
-        } catch (FileNotFoundException e) {
-            System.out.println("El archivo no se encontró");
-        } catch (IOException e) {
-            System.out.println(e);
-            System.out.println("Error al serializar el objeto");
-        }
-    }
+public class Serializador {
+	
+	  public static void serializar(Sistema sis) {
+	    	serializarBibliotecas(sis,new File("src\\baseDatos\\temp\\Bibliotecas.txt"));
+	    	serializarLibros(sis,new File("src\\baseDatos\\temp\\Libros.txt"));
+	    	serializarComputadores(sis,new File("src\\baseDatos\\temp\\Computadores.txt"));
+	    	serializarAutores(sis,new File("src\\baseDatos\\temp\\Autores.txt"));
+	    	serializarUsuario(sis, new File("src\\baseDatos\\temp\\Usuario.txt"));
+	    }
+	  
+	  public static void serializarBibliotecas(Sistema sis, File ruta) {
+		  try {
+			  FileOutputStream file = new FileOutputStream(ruta);
+	          ObjectOutputStream out = new ObjectOutputStream(file);
+	          out.writeObject(sis.getBibliotecas());
+	          out.close();
+	          file.close();
+		  } catch (IOException e){
+			  System.out.println("Error en la serializacion");
+			  
+		  }
+		  
+	  }
+	  
+	  public static void serializarLibros(Sistema sis, File ruta) {
+		  try {
+			  FileOutputStream file = new FileOutputStream(ruta);
+	          ObjectOutputStream out = new ObjectOutputStream(file);
+	          out.writeObject(sis.getLibros());
+	          out.close();
+	          file.close();
+		  } catch (IOException e){
+			  System.out.println("Error en la serializacion");
+			  
+		  }
+		  
+	  }
+	  
+	  public static void serializarComputadores(Sistema sis, File ruta) {
+		  try {
+			  FileOutputStream file = new FileOutputStream(ruta);
+	            ObjectOutputStream out = new ObjectOutputStream(file);
+	            out.writeObject(sis.getComputadores());
+	            out.close();
+	            file.close();
+		  } catch (IOException e){
+			  System.out.println("Error en la serializacion");
+			  
+		  }
+		  
+	  }
+	  
+	  public static void serializarAutores(Sistema sis, File ruta) {
+		  try {
+			  FileOutputStream file = new FileOutputStream(ruta);
+	            ObjectOutputStream out = new ObjectOutputStream(file);
+	            out.writeObject(sis.getAutores());
+	            out.close();
+	            file.close();
+		  } catch (IOException e){
+			  System.out.println("Error en la serializacion");
+			  
+		  }
+		  
+	  }
+	  
+	  public static void serializarUsuario(Sistema sis, File ruta) {
+		  try {
+			  FileOutputStream file = new FileOutputStream(ruta);
+	            ObjectOutputStream out = new ObjectOutputStream(file);
+	            out.writeObject(sis.getUser());
+	            out.close();
+	            file.close();
+		  } catch (IOException e){
+			  System.out.println("Error en la serializacion");
+			  
+		  }
+	  }
+	  
+	  
 }
-
