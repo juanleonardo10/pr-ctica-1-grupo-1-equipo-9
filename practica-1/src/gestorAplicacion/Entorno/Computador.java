53a94ab1bdfd597125f388cb91ad284364a440a6
danielh434 <dhincapieca@unal.edu.co>
2025-01-22 20:21:10 -0500

    Class Admdn 3

---
+++ practica-1/src/gestorAplicacion/Entorno/Computador.java
@@ -0,0 +1,56 @@
+package gestorAplicacion.Entorno;
+
+import java.util.ArrayList;
+import java.io.Serializable;
+
+public class Computador extends Recurso implements Serializable{
+	private static final long serialVersionUID = 1L;
+	static private int totalPCs;
+    private String marca;
+    private String gama;
+    
+
+    // Constructor de la clase Computador
+    public Computador(String nombre, int idRecurso, String marca, String gama) {
+        super(nombre, idRecurso); // Llama al constructor de la clase base (Recurso)
+        this.marca = marca;
+        this.gama = gama;
+        totalPCs++;
+    }
+    
+    public Computador() {
+    	this("Sin referencia", 0, "Sin marca", "Sin gama");
+    }
+
+    // Métodos para obtener y establecer la marca del computador
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    // Métodos para obtener y establecer la gama del computador
+    public String getGama() {
+        return gama;
+    }
+    
+    public static int getTotalPCs() {
+		return totalPCs;
+	}
+
+    public void setGama(String gama) {
+        this.gama = gama;
+    }
+    
+    public String tipoRecurso() {
+    	return "Computador";
+    }
+    
+    public String toString() {
+    	return this.getNombre();
+    }
+}
+
+
